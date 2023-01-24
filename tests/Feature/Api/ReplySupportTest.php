b1c833532a85a8340dfe067068de7c505b5b67c1
Mauricio Ferreira <mauriciovictor17@gmail.com>
2023-01-23 21:46:27 -0300

    test reply support

--- tests/Feature/Api/ReplySupportTest.php
+++ tests/Feature/Api/ReplySupportTest.php
@@ -2,21 +2,40 @@
 
 namespace Tests\Feature\Api;
 
+use App\Models\Support;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class ReplySupportTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example()
-    {
-        $response = $this->get('/');
+    use TestTrait;
 
-        $response->assertStatus(200);
+    public function test_create_reply_to_support_unauthenticated()
+    {
+        $response = $this->postJson('/replies');
+
+        $response->assertStatus(401);
+    }
+
+    public function test_create_reply_to_error_validations()
+    {
+        $response = $this->postJson('/replies', [], $this->defaultHeaders());
+
+        $response->assertStatus(422);
+    }
+
+    public function test_create_reply_to_support()
+    {
+
+        $support = Support::factory()->create();
+        $payload = [
+            'support_id' => $support->id,
+            'description' => "Description oloco meu"
+        ];
+
+        $response = $this->postJson('/replies', $payload, $this->defaultHeaders());
+
+        $response->assertStatus(201);
     }
 }
